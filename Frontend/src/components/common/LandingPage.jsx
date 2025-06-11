e58f4342cadadd5ed57bed05e535b20b6b75e410
GloriaAndonyaIT <gloriaandonyaa@gmail.com>
2025-06-11 19:43:55 +0300

    Changed nackground image

--- Frontend/src/components/common/LandingPage.jsx
+++ Frontend/src/components/common/LandingPage.jsx
@@ -74,7 +74,7 @@ const LandingPage = () => {
         id="home" 
         className="relative h-screen bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('https://pawapos.com/wp-content/uploads/2024/06/open-small-business-in-Kenya-1024x614.jpg')`
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('https://images.unsplash.com/photo-1651800314574-25ae6cfd1da0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`
         }}
       >
         <div className="absolute inset-0 flex items-center justify-start pl-8 md:pl-16 lg:pl-24">
